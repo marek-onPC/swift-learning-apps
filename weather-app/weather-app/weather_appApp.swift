be21bb6f2ee80ae2f7120a9613a3f84cd4deed81
Marek Smieja <mareksmieja@gmail.com>
2023-10-07 15:21:15 +0900

    [weather-app] Init commit

---
+++ weather-app/weather-app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather-app
+//
+//  Created by Marek on 2023/10/07.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
